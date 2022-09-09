b18f00f70a729a10c45f7a3d735682a9bd8d5750
Alex Moura <alekissi@gmail.com>
2022-09-09 16:33:32 -0300

    upload inicial

---
+++ AppMvcBasico/Models/Fornecedor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppMvcBasico.Models
+{
+    public class Fornecedor : Entity
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatorio.")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1}", MinimumLength = 2)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatorio.")]
+        [StringLength(14, ErrorMessage = "O campo {0} precisa ter {2} ou {1} digitos.", MinimumLength = 11)]
+        public string Documento { get; set; }
+
+        public TipoFornecedor TipoFornecedor { get; set; }
+
+        [DisplayName("Endereço")]
+        public Endereco Endereco { get; set; }
+
+        [DisplayName("Ativo?")]
+        public bool Ativo { get; set; }
+
+        /* EF Relations */
+        public IEnumerable<Produto> Produtos { get; set; }
+
+    }
+}
---
+++ AppMvcBasico/Models/Produto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppMvcBasico.Models
+{
+    public class Produto : Entity
+    {
+        public Guid FornecedorID { get; set; }
+
+        [Required(ErrorMessage ="O campo {0} é obrigatorio.")]
+        [StringLength(200, ErrorMessage ="O campo {0} precisa ter entre {2} e {1}", MinimumLength =2)]
+        public string Nome { get; set; }
+        
+        [Required(ErrorMessage = "O campo {0} é obrigatorio.")]
+        [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {2} e {1}", MinimumLength = 2)]
+        public string Descricao { get; set; }
+        
+        [Required(ErrorMessage = "O campo {0} é obrigatorio.")]
+        public string Imagem { get; set; }
+        
+        [Required(ErrorMessage = "O campo {0} é obrigatorio.")]
+        public decimal Valor { get; set; }
+        
+        public DateTime DataCadastro { get; set; }
+        
+        [Required(ErrorMessage = "O campo {0} é obrigatorio.")]
+        [DisplayName("Ativo?")]
+        public bool Ativo { get; set; }
+
+        /* EF Ralation */
+
+        public Fornecedor Fornecedor { get; set; }
+    }
+}
